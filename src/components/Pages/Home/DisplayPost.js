d0a2b7733da59df1edc869e0196c5d1b2b6ed122
Tauhidul Islam Robin <robin13690@gmail.com>
2022-12-28 11:58:00 +0600

    dispaly 3 post

--- src/components/Pages/Home/DisplayPost.js
+++ src/components/Pages/Home/DisplayPost.js
@@ -5,31 +5,35 @@ import { useQuery } from '@tanstack/react-query';
 
 const DisplayPost = () => {
 
+    const { data: allPosts = [], refetch, isLoading } = useQuery({
+        queryKey: ["allPosts"],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:4000/allPosts`);
+            const data = await res.json();
+            return data;
+        }
+    });
+    console.log(allPosts);
 
 
     return (
         <div>
+            <h1 className='text-5xl font-bold my-10 text-center'>Display Post Section</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                <div className="card w-full md:w-96 bg-base-100 shadow-xl">
-                    <figure className="">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
+                {
+                    allPosts.map(posts => <div className="card w-full md:w-96 bg-base-100 shadow-xl">
 
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        {
-                            <p>Commet part will come</p>
-                        }
-                        <div className="card-actions items-center">
-                            <Link><FaHeart className='text-red-500'></FaHeart></Link>
-                            <button className="btn btn-primary btn-sm"><Link>Details</Link></button>
-                            <button className="btn btn-primary btn-sm"><Link>Add comment</Link></button>
+                        <div className="card-body items-center text-center">
+
+                            <h2 className="card-title">Post About : <span></span></h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                            <div className="card-actions items-center">
+                                <button className="btn btn-primary btn-sm"><Link>Details</Link></button>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-
+                    </div>)
+                }
             </div>
         </div>
     );
